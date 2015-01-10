b02d509bd2243cb52b75ee33c3e90dffc430972a
Adrian Dawid <adriandwd@gmail.com>
2015-01-10 22:47:50 +0100

    * Added the "real" example code

--- GameSharingSample/Classes/HelloWorldScene.cpp
+++ GameSharingSample/Classes/HelloWorldScene.cpp
@@ -1,4 +1,5 @@
 #include "HelloWorldScene.h"
+#include "GameSharing.h"
 
 USING_NS_CC;
 
@@ -20,8 +21,6 @@ Scene* HelloWorld::createScene()
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
     if ( !Layer::init() )
     {
         return false;
@@ -30,47 +29,22 @@ bool HelloWorld::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+    auto clickMe = MenuItemLabel::create(LabelTTF::create("Click Me!", "Arial", 51),
+                                        [](Object *obj) {
+                                            GameSharing::SubmitScore(rand());
+                                            GameSharing::ShowLeaderboards();
+                                        });
+    
+	clickMe->setPosition(Vec2(origin.x + visibleSize.width /2  ,
+                                origin.y + visibleSize.height/2));
 
     // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
+    auto menu = Menu::create(clickMe, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = LabelTTF::create("Hello World", "Arial", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    GameSharing::initGameSharing();
     
     return true;
 }
